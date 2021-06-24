e996192067b2e297d98261846a43cb84a509e950
ValeriiaGW <valeriia.b@gowombat.team>
2021-06-24 16:59:00 +0300

    hw4

--- HW4/hw_4.py
+++ HW4/hw_4.py
@@ -1,46 +1,48 @@
 
-# my_list = [2, 4, 56, 76, 66, 656, 544, 333, 234]
-# for i in my_list:
-#     if i > 100:
-#         print(i)
+my_list = [2, 4, 56, 76, 66, 656, 544, 333, 234]
+for i in my_list:
+    if i > 100:
+        print(i)
 
 ###########################################################
 
-# my_list = [2, 4, 56, 76, 66, 656, 544, 333, 234]
-# my_results = []
-# for i in my_list:
-#     if i > 100:
-#         new_list = i
-#         my_results.append(new_list)
-# print(my_results)
+my_list = [2, 4, 56, 76, 66, 656, 544, 333, 234]
+my_results = []
+for i in my_list:
+    if i > 100:
+        new_list = i
+        my_results.append(new_list)
+print(my_results)
 
 ###########################################################
 
-# my_list = [435]
-# if len(my_list) >= 2:
-#     my_list.append(my_list[-1] + my_list[-2])
-# else:
-#     my_list.append(0)
-# print(my_list)
+my_list = [435]
+if len(my_list) >= 2:
+    my_list.append(my_list[-1] + my_list[-2])
+else:
+    my_list.append(0)
+print(my_list)
 
 ###########################################################
 
-# value = input("введи дробное число: ")
-# try:
-#     value = float(value)
-#     new_value = value ** -1
-# except (ValueError, ZeroDivisionError):
-#     print("Введено не число или ноль")
-#     exit(1)
-# print(new_value)
+value = input("введи дробное число: ")
+try:
+    value = float(value)
+    new_value = value ** -1
+except (ValueError, ZeroDivisionError):
+    print("Введено не число или ноль")
+    exit(1)
+print(new_value)
 
 ###########################################################
 
-# my_string = '0123456789'
-# for s1 in my_string:
-# 	for s2 in my_string:
-# 		new_list = int(s1 + s2)
-# 		print(new_list)
+
+my_string = '0123456789'
+result = list()
+for s1 in my_string:
+	for s2 in my_string:
+		result.append(int(s1 + s2))
+print(result)
 
 
 
