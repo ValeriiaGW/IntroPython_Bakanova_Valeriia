2ad71ec9f9f7bac917432091fda53a1c7ff6573e
ValeriiaGW <valeriia.b@gowombat.team>
2021-08-03 22:09:43 +0300

    hw_12

--- HW_12/hw_12.py
+++ HW_12/hw_12.py
@@ -16,9 +16,48 @@ filename = "data.json"
 
 def read_data(filename):
     with open(filename) as json_file:
-        json.load(json_file)
-
+        data = json.load(json_file)
 
     return data
 
-print(read_data("data.json"))
+#
+# # print(read_data("data.json"))
+#
+# def get_names(data):
+#     # names = data.get("name").split(" ")
+#     # return [name[-1] for name in names]
+#
+#     full_names = [dictionary.get("name") for dictionary in data]
+#     name = [name[-1] for name in [name.split() for name in full_names]]
+#     return name
+#
+#
+# print(sorted(read_data("data.json"), key=get_names))
+
+
+# def sort_name(data):
+#     sorted_list = sorted(read_data("data.json"), key=get_names)
+#     return sorted_list
+# #
+# #
+# print(sort_name(get_names(read_data("data.json"))))
+
+
+
+# def sort_by_text_len(data):
+#     return len(data['text'])
+#
+#
+def get_name(data):
+    name = data.get('name').split(' ')
+    if len(name) == 1:
+        return name[0]
+    else:
+        return name[-1]
+
+
+def sort_by_name(data):
+    return sorted(data, key=get_name)
+# print(sorted(data, key=sort_by_text_len))
+
+print(sort_by_name(read_data("data.json")))
